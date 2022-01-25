ca22a591c839e740ecab7ec4b33c814c6a33d818
Rowan Biets <rowan.biets@student.ehb.be>
2022-01-25 11:59:17 +0100

    A NEW CHAPTER

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function showSlides() {
     if (slideIndex > slides.length) {
         slideIndex = 1;
     }
-    slides[slideIndex - 1].display = "block";
+    slides[slideIndex - 1].style.display = "block";
     setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
